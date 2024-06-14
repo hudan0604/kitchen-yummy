53c26ad6e6e0acf8d7f5ee32650aef8a5df09ef4
Nicolas Hudavert <hudanico@hotmail>
2024-06-14 17:20:27 +0200

    style: listing

--- src/app/shared/data/data.ts
+++ src/app/shared/data/data.ts
@@ -7,5 +7,100 @@ export const data = {
       'assets/images/beef_thai_salad.jpg',
       false
     ),
+    new RecipeModelClass(
+      'Soupe de poulet à la citronelle et lait de coco',
+      'assets/images/chicken_coco_greenlemon_soup.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Curry de poulet au basilic thaï',
+      'assets/images/chicken_curry_basilic.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
+    new RecipeModelClass(
+      'Salade de boeuf thaïe',
+      'assets/images/beef_thai_salad.jpg',
+      false
+    ),
   ],
 };
